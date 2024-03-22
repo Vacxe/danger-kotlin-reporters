cf8009e27daec4e80f07410cfb0c0667ec24a84e
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-03-22 19:04:43 +1000

    add dependency to core

--- shellcheck/build.gradle.kts
+++ shellcheck/build.gradle.kts
@@ -8,8 +8,7 @@ group = "io.github.vacxe.danger.kotlin"
 
 dependencies {
     implementation("systems.danger:danger-kotlin-sdk:1.2")
+    implementation("io.github.vacxe.danger.kotlin:core:1.0.0")
 
     implementation("com.google.code.gson:gson:2.9.1")
-
-    compileOnly(project(":core"))
 }
--- yamllint/build.gradle.kts
+++ yamllint/build.gradle.kts
@@ -11,9 +11,7 @@ group = "io.github.vacxe.danger.kotlin"
 
 dependencies {
     implementation("systems.danger:danger-kotlin-sdk:1.2")
-
-    compileOnly(project(":core"))
-    //implementation(project(":core"))
+    implementation("io.github.vacxe.danger.kotlin:core:1.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
